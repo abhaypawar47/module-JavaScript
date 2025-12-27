59c7ef61cc9e394cecab2a1ffa601e2ef3165386
Abhay Pawar <abhaypawar0407@gmail.com>
2025-12-27 22:00:19 +0530

    changes made

---
+++ REG EX.js
@@ -0,0 +1,23 @@
+//? Using the Test Method
+// let testStr = "freecodeCamp";
+// let testRegx = /code/;
+// console.log(testRegx.test(testStr));
+//!-----
+//? Match Literal Strings
+// let testStr = "hello my name is Kelvin";
+// let testRegx = /Kelvin/;
+// console.log(testRegx.test(testStr));
+//!-----
+//? Match a Literal String with Different Possibilities
+// let petString = "James has a pet cat.";
+// let petRegex = /dog|cat|bird|fish/; // Change this line
+// let result = petRegex.test(petString);
+// console.log(result);
+//!-----
+//? Ignore Case While Matching
+// let myString = "freeCodeCamp";
+// let fccRegex = /camp/i; // Change this line
+// let result = fccRegex.test(myString);
+// console.log(result);
+//!-----
+

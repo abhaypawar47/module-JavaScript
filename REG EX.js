a7541c5d20aec274857e70f32da6324840acd59a
Abhay Pawar <abhaypawar0407@gmail.com>
2025-12-31 21:59:04 +0530

    commit

--- REG EX.js
+++ REG EX.js
@@ -101,8 +101,4 @@
 
 // console.log(jennyStr.match(myRegex));
 
-//? problem
-let quoteSample = "Blueberry 3.141592653s are delicious.";
-let myRegex = /[a-z0-9]/gi; // Change this line
-let result = quoteSample.match(myRegex); // Change this line
-console.log(result);
+//? problem////
